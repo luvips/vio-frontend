5447abe0fb94096b4fdff28b4d156a576da47138
luvia <luviaamagaali07@gmail.com>
2026-03-05 10:10:40 -0600

    centraliza exportaciones de tmdb

---
+++ src/lib/tmdb/index.ts
@@ -0,0 +1,7 @@
+export * from "./movies";
+export * from "./images";
+export * from "./genres";
+export * from "./config";
+export type { Movie, MovieList } from "@/schemas/movie";
+export type { MovieDetail } from "@/schemas/movie-detail";
+export type { CastMember, MovieCredits } from "@/schemas/credits";
